377e5ca1a2c5fc2c8bc7e5c7b9717504cadd3f05
Saber Goob <saber.goob14@gmail.com>
2022-10-11 12:48:11 +0100

    frist commit

---
+++ src/UserList.js
@@ -0,0 +1,32 @@
+import React, {useEffect,useState} from 'react'
+import axios from "axios";
+import './userlist.css'
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then((res)=>setUsers(res.data))
+            .catch((err) => console.log(err));
+      
+    }, []);
+  return (
+    <div className='userlist'>
+        {users.map((user) =>(
+            <div className='cardlist'>
+                <div className='title'><span>Name :</span> {user.name}</div>
+                <div className='subtitle'> <span>UserName :</span> {user.username}</div>
+                <div className='adress'> <span>Adress :</span> {user.address.city}</div>
+                <div className='gmail'> <span>Email :</span> {user.email}</div>
+
+            </div>
+
+
+
+        ))
+        }
+        </div>
+  )
+}
+
+export default UserList
